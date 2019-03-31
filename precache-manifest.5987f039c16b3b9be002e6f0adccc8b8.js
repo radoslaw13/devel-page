39a1b0c477f9e631c65ac58b19b88f656f7cd7bb
Radek <rad.musial@gmail.com>
2019-03-31 16:39:28 +0200

    Updates

---
+++ precache-manifest.5987f039c16b3b9be002e6f0adccc8b8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "008455ffeffde629258cd9d0f52b24ce",
+    "url": "/devel-page/static/media/bg-header.008455ff.jpg"
+  },
+  {
+    "revision": "1bb0f96501701332f04a",
+    "url": "/devel-page/static/css/main.36b287ac.chunk.css"
+  },
+  {
+    "revision": "b31347d68fcd9033d810",
+    "url": "/devel-page/static/js/runtime~main.d8caed06.js"
+  },
+  {
+    "revision": "d830f4fcf168916a2059125f47313d43",
+    "url": "/devel-page/static/media/counter-bg.d830f4fc.jpg"
+  },
+  {
+    "revision": "a85ceae4f62e5d4e7ce8",
+    "url": "/devel-page/static/js/2.644b9aa9.chunk.js"
+  },
+  {
+    "revision": "5a460d787f58bd17758ea4afb87ef091",
+    "url": "/devel-page/static/media/project1.5a460d78.jpg"
+  },
+  {
+    "revision": "6b6044025d7ff5fec3a58808e1c73553",
+    "url": "/devel-page/static/media/project2.6b604402.jpg"
+  },
+  {
+    "revision": "44fe10fa5584cdc9ffa3b333ae4242ed",
+    "url": "/devel-page/static/media/project3.44fe10fa.jpg"
+  },
+  {
+    "revision": "7d9e620aec8407bbfc37c79833758c82",
+    "url": "/devel-page/static/media/project4.7d9e620a.png"
+  },
+  {
+    "revision": "1bb0f96501701332f04a",
+    "url": "/devel-page/static/js/main.011f024e.chunk.js"
+  },
+  {
+    "revision": "a06a3248f173f51bcbbcdd434a1de129",
+    "url": "/devel-page/static/media/pattern.bg.a06a3248.png"
+  },
+  {
+    "revision": "ae762b905d5cd60d5d366d54395fbba6",
+    "url": "/devel-page/static/media/testimonials-bg.ae762b90.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/devel-page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/devel-page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/devel-page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/devel-page/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a85ceae4f62e5d4e7ce8",
+    "url": "/devel-page/static/css/2.46e05105.chunk.css"
+  },
+  {
+    "revision": "9861ea586baa37058ff959b1cd69d4b5",
+    "url": "/devel-page/index.html"
+  }
+];
